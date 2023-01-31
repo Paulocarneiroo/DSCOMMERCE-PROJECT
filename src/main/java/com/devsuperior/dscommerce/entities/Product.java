3dbcb043b1892ad5674bc5898551f2b62a8a959e
Paulo Carneiro <paulocarneirosegundo@gmail.com>
2023-01-30 23:13:59 -0300

    GET product by id

--- src/main/java/com/devsuperior/dscommerce/entities/Product.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Product.java
@@ -16,6 +16,7 @@ public class Product {
     private String description;
     private Double price;
     private String imgUrl;
+
     @ManyToMany
     @JoinTable(name = "tb_product_category",
     joinColumns = @JoinColumn(name = "product_id"),
