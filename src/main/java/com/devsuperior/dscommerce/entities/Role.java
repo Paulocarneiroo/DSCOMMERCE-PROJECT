87646f98dbe8cad01d99a601f27ec3cc1c1851cc
Paulo Carneiro <paulocarneirosegundo@gmail.com>
2023-10-28 13:19:44 -0300

    UserService and UserRepository implementation

--- src/main/java/com/devsuperior/dscommerce/entities/Role.java
+++ src/main/java/com/devsuperior/dscommerce/entities/Role.java
@@ -1,10 +1,14 @@
 package com.devsuperior.dscommerce.entities;
 
+import org.springframework.security.core.GrantedAuthority;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -16,15 +20,13 @@ public class Role {
     public Long getId() {
         return id;
     }
-
     public void setId(Long id) {
         this.id = id;
     }
-
+    @Override
     public String getAuthority() {
         return authority;
     }
-
     public void setAuthority(String authority) {
         this.authority = authority;
     }
